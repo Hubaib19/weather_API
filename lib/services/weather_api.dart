0a8817189a36782904e1114e86da047119860ab6
Hubaib19 <hubaib0019@gmail.com>
2024-02-06 09:39:04 +0530

    first commit

--- lib/services/weather_api.dart
+++ lib/services/weather_api.dart
@@ -7,10 +7,10 @@ import 'package:weatherapi/model/weather_model.dart';
 class Service {
   Future<Weather?> getCurrentWeather(String? location) async {
     try {
-      var endpoint = Uri.parse(
+      var url = Uri.parse(
           "https://api.openweathermap.org/data/2.5/weather?q=$location&appid=3837f082b65893f0fc5883912ebd9ac8&units=metric");
 
-      var response = await http.get(endpoint);
+      var response = await http.get(url);
 
       if (response.statusCode == 200) {
         var body = jsonDecode(response.body);
